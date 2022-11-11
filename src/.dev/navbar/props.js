45d7b911743e87192e25c2550eb2097ad765509e
johtoLogmaster <96054483+johtoLogmaster@users.noreply.github.com>
2022-11-11 20:31:08 +0800

    chore: fixed menu on localhost

--- src/.dev/navbar/props.js
+++ src/.dev/navbar/props.js
@@ -1,4 +1,12 @@
 const config = require('../../app/config.json');
+function getItems(){
+    return config.items.map(function(item) {
+        return {
+            name: item.url?.replace('dist', ''),
+            labelText: item.menuName,
+        }
+    });
+}
 /**
  * Props item - Houses all the navbar items and submenu items
  */
@@ -11,16 +19,17 @@ const props = [
              en: 'Activity'
             },
          hasSubmenu: true,
-         submenuItems: [
-                {
-                    name: config.items[0].url,
-                    labelText: config.items[0].menuName,
-                },
-                {
-                    name: config.items[1].url,
-                    labelText: config.items[1].menuName,
-                }
-            ]
+         submenuItems : getItems()
+        //  submenuItems: [
+        //         {
+        //             name: config.items[0].url,
+        //             labelText: config.items[0].menuName,
+        //         },
+        //         {
+        //             name: config.items[1].url,
+        //             labelText: config.items[1].menuName,
+        //         }
+        //     ]
     },
     
     
